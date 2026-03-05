2ffaef936f911207c7e33c027e8c3def97fe5ce5
Leandro <leandrinsilva22@gmail.com>
2026-03-05 15:08:52 -0300

    style - improve form

--- frontend/src/components/report/ReportTable.vue
+++ frontend/src/components/report/ReportTable.vue
@@ -11,43 +11,47 @@ defineProps<{
 
 <template>
     <CardContainer v-if="items.length" size="lg">
-        <table class="w-full text-sm">
-            <thead class="border-b">
-                <tr class="text-left">
-                    <th class="p-2">Nome</th>
-                    <th class="p-2">Registros</th>
-                    <th class="p-2">Processados</th>
-                    <th class="p-2">Iniciado</th>
-                    <th class="p-2">Finalizado</th>
-                    <th class="p-2">Status</th>
-                    <th class="p-2">Arquivo</th>
-                </tr>
-            </thead>
+        <div class="overflow-x-auto border rounded-lg shadow-sm">
+            <table class="table-fixed w-full border-collapse">
+                <thead class="bg-gray-100 text-left text-gray-700">
+                    <tr>
+                        <th class="p-2 w-56 truncate" title="Nome do relatório">Nome</th>
+                        <th class="p-2 w-36">Registros</th>
+                        <th class="p-2 w-36">Processados</th>
+                        <th class="p-2 w-56">Iniciado</th>
+                        <th class="p-2 w-56">Finalizado</th>
+                        <th class="p-2 w-28">Status</th>
+                        <th class="p-2 w-40 truncate" title="Arquivo final do relatório">Arquivo</th>
+                    </tr>
+                </thead>
 
-            <tbody>
-                <tr v-for="item in items" :key="item.id" class="border-b">
-                    <td class="p-2">{{ item.report_name }}</td>
-                    <td class="p-2">{{ item.total_records }}</td>
-                    <td class="p-2">{{ item.processed_records }}%</td>
+                <tbody class="divide-y divide-gray-200">
+                    <tr v-for="item in items" :key="item.id" class="hover:bg-gray-50 text-left">
+                        <td class="p-2 truncate" :title="item.report_name">{{ item.report_name }}</td>
+                        <td class="p-2">{{ item.total_records }}</td>
+                        <td class="p-2">{{ item.processed_records }}%</td>
 
-                    <td class="p-2">{{ item.started_at ?? '-' }}</td>
-                    <td class="p-2">{{ item.finished_at ?? '-' }}</td>
+                        <td class="p-2">{{ item.started_at ?? '-' }}</td>
+                        <td class="p-2">{{ item.finished_at ?? '-' }}</td>
 
-                    <td class="p-2">
-                        <ReportStatusBadge :text="item.status_name" :status-id="item.status_id" />
-                    </td>
+                        <td class="p-2">
+                            <ReportStatusBadge :text="item.status_name" :status-id="item.status_id" />
+                        </td>
 
-                    <td class="p-2">
-                        <BaseButton v-if="item.final_file_path" size="sm" as="a" variant="outline"
-                            :href="`/storage/${item.final_file_path}`">
-                            Download
-                        </BaseButton>
-
-                        <span v-else>-</span>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+                        <td class="p-2">
+                            <BaseButton v-if="item.final_file_path" size="sm" as="a" variant="outline"
+                                :href="`/storage/${item.final_file_path}`">
+                                Download
+                            </BaseButton>
+                            <span v-else>-</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </CardContainer>
-    <p v-else-if="!loading" class="text-center text-gray-500">Nenhum resultado para a busca.</p>
+
+    <p v-else-if="!loading" class="text-center text-gray-500">
+        Nenhum resultado para a busca.
+    </p>
 </template>
